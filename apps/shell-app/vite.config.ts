18a8621200e6b95963053120cce43b1c11dac276
victormanuel-12 <victormanuel-12@users.noreply.github.com>
2026-05-03 22:58:50 -0500

    feat: add GitHub Actions workflows for test coverage and publishing reports

--- apps/shell-app/vite.config.ts
+++ apps/shell-app/vite.config.ts
@@ -1,36 +1,36 @@
 /// <reference types="vitest" />
 
-import { defineConfig } from 'vitest/config'
-import react from '@vitejs/plugin-react'
-import federation from '@originjs/vite-plugin-federation'
+import { defineConfig } from "vitest/config";
+import react from "@vitejs/plugin-react";
+import federation from "@originjs/vite-plugin-federation";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     federation({
-      name: 'shell_app',
+      name: "shell_app",
       // Aquí le decimos dónde está el microfrontend del dashboard
       remotes: {
-        dashboardApp: 'http://localhost:3001/assets/remoteEntry.js',
-        flotaApp: 'http://localhost:4173/assets/remoteEntry.js',
-        contratosApp: 'http://localhost:4174/assets/remoteEntry.js',
+        dashboardApp: "http://localhost:3001/assets/remoteEntry.js",
+        flotaApp: "http://localhost:4173/assets/remoteEntry.js",
+        contratosApp: "http://localhost:4174/assets/remoteEntry.js",
       },
-      shared: ['react', 'react-dom', 'recharts'] // Compartimos React, ReactDOM y Recharts para evitar cargar varias versiones en la app final
-    })
+      shared: ["react", "react-dom", "recharts"], // Compartimos React, ReactDOM y Recharts para evitar cargar varias versiones en la app final
+    }),
   ],
   server: {
     port: 3000,
   },
   build: {
     modulePreload: false,
-    target: 'esnext',
+    target: "esnext",
     minify: false,
     cssCodeSplit: false,
   },
   test: {
     globals: true,
-    environment: 'jsdom',
-    setupFiles: './vitest.setup.ts',
-  }
-})
+    environment: "jsdom",
+    setupFiles: "./vitest.setup.ts",
+  },
+});
